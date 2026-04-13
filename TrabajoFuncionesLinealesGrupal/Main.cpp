63f1b048027ec977e467a880baec7ad870124fa5
Lincoln Angel Aranda Eugenio <lincolnangelarandaeugenio@gmail.com>
2026-04-13 15:52:24 -0500

    Modificacion del codigo desde una cuenta externa

--- TrabajoFuncionesLinealesGrupal/Main.cpp
+++ TrabajoFuncionesLinealesGrupal/Main.cpp
@@ -2,7 +2,7 @@
 //Este codigo no se debe modificar nuevamente
 using namespace System;
 using namespace System::Windows::Forms;
-
+//Este es un cambio para medir los pull y push desde la cuenta de mis compañeros
 [STAThreadAttribute]
 int main(array<String^>^ args) {
 	Application::EnableVisualStyles();
