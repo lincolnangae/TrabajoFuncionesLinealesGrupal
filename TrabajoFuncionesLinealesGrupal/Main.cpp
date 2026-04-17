3feca823038354185d2fbb96936d634c5c00648b
TpSYCHO777 <estudiosjrchp@gmail.com>
2026-04-17 14:58:39 -0500

    diseño de la intefaz general

--- TrabajoFuncionesLinealesGrupal/Main.cpp
+++ TrabajoFuncionesLinealesGrupal/Main.cpp
@@ -1,14 +1,10 @@
 #include "Program.h"
-//Este codigo no se debe modificar nuevamente
 using namespace System;
 using namespace System::Windows::Forms;
-//Este es un cambio para medir los pull y push desde la cuenta de mis compañeros
 [STAThreadAttribute]
 int main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
-
-	//Este es el formulario principal de la aplicación
 	TrabajoFuncionesLinealesGrupal::Program form;
 	Application::Run(% form);
 	return 0;
