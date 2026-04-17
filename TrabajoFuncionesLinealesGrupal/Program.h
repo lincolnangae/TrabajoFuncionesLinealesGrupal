4fc67b8edae7fbbd7c0ae96007f7f2cb145bda3d
Lincoln Angel Aranda Eugenio <lincolnangelarandaeugenio@gmail.com>
2026-04-17 15:10:36 -0500

    requerimientos

--- TrabajoFuncionesLinealesGrupal/Program.h
+++ TrabajoFuncionesLinealesGrupal/Program.h
@@ -48,8 +48,370 @@ namespace TrabajoFuncionesLinealesGrupal {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+<<<<<<< Updated upstream
 			this->SuspendLayout();
 			// 
+=======
+			this->btnConfirmCord = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->textCoordY = (gcnew System::Windows::Forms::TextBox());
+			this->textCoorX = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->groupBox3 = (gcnew System::Windows::Forms::GroupBox());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->groupBox4 = (gcnew System::Windows::Forms::GroupBox());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->groupBox5 = (gcnew System::Windows::Forms::GroupBox());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->textBox5 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox6 = (gcnew System::Windows::Forms::TextBox());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			this->groupBox3->SuspendLayout();
+			this->groupBox4->SuspendLayout();
+			this->groupBox5->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// btnConfirmCord
+			// 
+			this->btnConfirmCord->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->btnConfirmCord->Location = System::Drawing::Point(22, 88);
+			this->btnConfirmCord->Name = L"btnConfirmCord";
+			this->btnConfirmCord->Size = System::Drawing::Size(201, 32);
+			this->btnConfirmCord->TabIndex = 0;
+			this->btnConfirmCord->Text = L"Crear punto";
+			this->btnConfirmCord->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(19, 21);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(18, 16);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"X:";
+			this->label1->Click += gcnew System::EventHandler(this, &Program::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(19, 51);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(19, 16);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"Y:";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->textCoordY);
+			this->groupBox1->Controls->Add(this->textCoorX);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->btnConfirmCord);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Location = System::Drawing::Point(928, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(242, 130);
+			this->groupBox1->TabIndex = 3;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"COORDENADAS";
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &Program::groupBox1_Enter);
+			// 
+			// textCoordY
+			// 
+			this->textCoordY->Location = System::Drawing::Point(65, 51);
+			this->textCoordY->Name = L"textCoordY";
+			this->textCoordY->Size = System::Drawing::Size(135, 22);
+			this->textCoordY->TabIndex = 5;
+			// 
+			// textCoorX
+			// 
+			this->textCoorX->Location = System::Drawing::Point(65, 18);
+			this->textCoorX->Name = L"textCoorX";
+			this->textCoorX->Size = System::Drawing::Size(135, 22);
+			this->textCoorX->TabIndex = 4;
+			this->textCoorX->TextChanged += gcnew System::EventHandler(this, &Program::textCoorX_TextChanged);
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->comboBox1);
+			this->groupBox2->Controls->Add(this->button1);
+			this->groupBox2->Location = System::Drawing::Point(928, 148);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(242, 108);
+			this->groupBox2->TabIndex = 4;
+			this->groupBox2->TabStop = false;
+			this->groupBox2->Text = L"FiGURAS PREESTABLECIDAS";
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Location = System::Drawing::Point(22, 36);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(201, 24);
+			this->comboBox1->TabIndex = 1;
+			// 
+			// button1
+			// 
+			this->button1->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button1->Location = System::Drawing::Point(22, 70);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(201, 30);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Aceptar";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::SystemColors::Window;
+			this->panel1->Location = System::Drawing::Point(12, 12);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(910, 739);
+			this->panel1->TabIndex = 5;
+			// 
+			// groupBox3
+			// 
+			this->groupBox3->Controls->Add(this->button6);
+			this->groupBox3->Controls->Add(this->button4);
+			this->groupBox3->Controls->Add(this->button5);
+			this->groupBox3->Controls->Add(this->button3);
+			this->groupBox3->Controls->Add(this->button2);
+			this->groupBox3->Location = System::Drawing::Point(928, 262);
+			this->groupBox3->Name = L"groupBox3";
+			this->groupBox3->Size = System::Drawing::Size(242, 131);
+			this->groupBox3->TabIndex = 6;
+			this->groupBox3->TabStop = false;
+			this->groupBox3->Text = L"REFLEXION";
+			// 
+			// button6
+			// 
+			this->button6->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button6->Location = System::Drawing::Point(65, 93);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(113, 30);
+			this->button6->TabIndex = 7;
+			this->button6->Text = L"Y = -X";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button4->Location = System::Drawing::Point(123, 57);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(113, 30);
+			this->button4->TabIndex = 5;
+			this->button4->Text = L"Origen";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button5->Location = System::Drawing::Point(6, 57);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(113, 30);
+			this->button5->TabIndex = 4;
+			this->button5->Text = L"Bisectriz";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button3->Location = System::Drawing::Point(123, 21);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(113, 30);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Eje Y";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button2->Location = System::Drawing::Point(6, 21);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(113, 30);
+			this->button2->TabIndex = 2;
+			this->button2->Text = L"Eje X";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// groupBox4
+			// 
+			this->groupBox4->Controls->Add(this->button7);
+			this->groupBox4->Controls->Add(this->textBox3);
+			this->groupBox4->Controls->Add(this->label5);
+			this->groupBox4->Controls->Add(this->textBox1);
+			this->groupBox4->Controls->Add(this->textBox2);
+			this->groupBox4->Controls->Add(this->label3);
+			this->groupBox4->Controls->Add(this->label4);
+			this->groupBox4->Location = System::Drawing::Point(928, 399);
+			this->groupBox4->Name = L"groupBox4";
+			this->groupBox4->Size = System::Drawing::Size(242, 154);
+			this->groupBox4->TabIndex = 7;
+			this->groupBox4->TabStop = false;
+			this->groupBox4->Text = L"HOMOTECIA";
+			// 
+			// button7
+			// 
+			this->button7->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button7->Location = System::Drawing::Point(22, 116);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(201, 30);
+			this->button7->TabIndex = 2;
+			this->button7->Text = L"Aceptar";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(65, 88);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(135, 22);
+			this->textBox3->TabIndex = 7;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(19, 88);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(18, 16);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"Z:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(65, 51);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(135, 22);
+			this->textBox1->TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(65, 18);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(135, 22);
+			this->textBox2->TabIndex = 4;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(19, 21);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(18, 16);
+			this->label3->TabIndex = 1;
+			this->label3->Text = L"X:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(19, 54);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(19, 16);
+			this->label4->TabIndex = 2;
+			this->label4->Text = L"Y:";
+			this->label4->Click += gcnew System::EventHandler(this, &Program::label4_Click);
+			// 
+			// groupBox5
+			// 
+			this->groupBox5->Controls->Add(this->button8);
+			this->groupBox5->Controls->Add(this->textBox4);
+			this->groupBox5->Controls->Add(this->label6);
+			this->groupBox5->Controls->Add(this->textBox5);
+			this->groupBox5->Controls->Add(this->textBox6);
+			this->groupBox5->Controls->Add(this->label7);
+			this->groupBox5->Controls->Add(this->label8);
+			this->groupBox5->Location = System::Drawing::Point(928, 559);
+			this->groupBox5->Name = L"groupBox5";
+			this->groupBox5->Size = System::Drawing::Size(242, 152);
+			this->groupBox5->TabIndex = 8;
+			this->groupBox5->TabStop = false;
+			this->groupBox5->Text = L"ROTACION";
+			// 
+			// button8
+			// 
+			this->button8->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button8->Location = System::Drawing::Point(22, 116);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(201, 30);
+			this->button8->TabIndex = 8;
+			this->button8->Text = L"Aceptar";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(65, 88);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(135, 22);
+			this->textBox4->TabIndex = 7;
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(19, 88);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(19, 16);
+			this->label6->TabIndex = 6;
+			this->label6->Text = L"Y:";
+			// 
+			// textBox5
+			// 
+			this->textBox5->Location = System::Drawing::Point(65, 51);
+			this->textBox5->Name = L"textBox5";
+			this->textBox5->Size = System::Drawing::Size(135, 22);
+			this->textBox5->TabIndex = 5;
+			// 
+			// textBox6
+			// 
+			this->textBox6->Location = System::Drawing::Point(65, 18);
+			this->textBox6->Name = L"textBox6";
+			this->textBox6->Size = System::Drawing::Size(135, 22);
+			this->textBox6->TabIndex = 4;
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(7, 21);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(52, 16);
+			this->label7->TabIndex = 1;
+			this->label7->Text = L"Angulo:";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(19, 54);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(18, 16);
+			this->label8->TabIndex = 2;
+			this->label8->Text = L"X:";
+			// 
+			// button9
+			// 
+			this->button9->ForeColor = System::Drawing::SystemColors::ActiveCaptionText;
+			this->button9->Location = System::Drawing::Point(934, 717);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(236, 30);
+			this->button9->TabIndex = 9;
+			this->button9->Text = L"Regresar";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+>>>>>>> Stashed changes
 			// Program
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
